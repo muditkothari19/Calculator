a41b6513a0f5b73746d1781f79626cc3e6f43e54
Mudit Kothari <127036380+muditkothari19@users.noreply.github.com>
2023-10-27 22:05:15 +0530

    Add files via upload

---
+++ basic_calculator_function.py
@@ -0,0 +1,26 @@
+def basic_calculator(a,b,operation):
+
+  if (a.isnumeric() & b.isnumeric()):
+    a=float(a)
+    b=float(b)
+    if operation == "add":
+      result = a + b
+    elif operation == "subtract":
+      result = a - b
+    elif operation == "divide":
+      result = a / b
+    elif operation == "multiply":
+      result = a * b
+    elif operation == "module":
+      result = a // b
+    elif operation == "Exponent":
+      result = a ** b
+      
+    else:
+      result = "Operations supported: add, subtract, divide, multiple only"
+    
+  else:
+    result = "Please enter a valid number for a & b"
+    
+
+  return result
